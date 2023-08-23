1e35e10693c5b9d32bd34a54ff0d93bfeef85957
Alihh961 <hajhassan.ali@outlook.com>
2023-08-23 12:07:36 +0200

    first step js

---
+++ script/javascript.js
@@ -0,0 +1,96 @@
+let buttons = document.querySelectorAll(".button");
+let previous = document.querySelector(".previous");
+let current = document.querySelector(".current");
+
+let numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+let operatorArray = ["/", "*", "-", "+"];
+current.innerHTML = 0;
+let equalOperationIsOn = false; // check if the last clicked caused an equal operation to reset the total if a number button is clicked
+let lastClick = "";
+
+// Mouse or touch
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    let buttonClicked = event.target.innerHTML;
+    if (lastClick == "=" && buttonClicked == "=") {
+      // if we click equal two times in a row
+      //ignore the click
+    } else {
+      if (numberArray.includes(buttonClicked)) {
+        // if the button is a number
+        if (operatorArray.includes(getLastCharacter(previous))) {
+          previous.innerHTML = eval(previous.innerHTML + current.innerHTML);
+          previous.innerHTML += buttonClicked;
+          console.log("includes");
+        } else {
+          current.innerHTML = buttonClicked;
+        }
+      } else if (isAnOperator(buttonClicked)) {
+        // if the clicked button is an Operator
+        previous.innerHTML = current.innerHTML + buttonClicked;
+        current.innerHTML = "";
+      } else if (buttonClicked == "AC") {
+        // if the button is AC
+        current.innerHTML = "0";
+        previous.innerHTML = "";
+      } else if (buttonClicked == "=") {
+        // ifthe button is Equal
+        lastClick = buttonClicked;
+        previous.innerHTML += current.innerHTML;
+        current.innerHTML = eval(previous.innerHTML);
+      }
+    }
+  });
+});
+
+
+
+
+
+// Functions 
+
+function isAnOperator(value) {
+    array = ["+", "-", "*", "/"];
+    if (array.includes(value)) {
+      return true;
+    }
+    return false;
+  }
+  
+  function lastCharacterIsAnOperator(value) {
+    arrayOfValue = value.toString().split("");
+  
+    if (isAnOperator(arrayOfValue[arrayOfValue.length - 1])) {
+      return true;
+    }
+    return false;
+  }
+  
+  function containsAnOperator(value) {
+    arrayOfValue = value.toString().split("");
+    if (
+      arrayOfValue.includes("-") ||
+      arrayOfValue.includes("+") ||
+      arrayOfValue.includes("*") ||
+      arrayOfValue.includes("/")
+    ) {
+      return true;
+    }
+    return false;
+  }
+  
+  function getLastCharacter(value) {
+    arrayOfValue = value.toString().split("");
+    let lastValue = arrayOfValue[arrayOfValue.length - 1];
+    return lastValue;
+  }
+  
+  function containsADecimal(value) {
+    arrayOfValue = value.toString().split("");
+    if (arrayOfValue.includes(".")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
